9a2c4abc2a5b8d887be86cba2180b5b71a9338a0
Abraham2143 <ab.neira@duocuc.cl>
2026-09-09 22:07:18 -0300

    chore: configura proyecto Spring Boot de shipments

---
+++ src/main/java/com/rutaexpress/ms_rutaexpress_shipments/MsRutaexpressShipmentsApplication.java
@@ -0,0 +1,13 @@
+package com.rutaexpress.ms_rutaexpress_shipments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRutaexpressShipmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRutaexpressShipmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rutaexpress/ms_rutaexpress_shipments/MsRutaexpressShipmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutaexpress.ms_rutaexpress_shipments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRutaexpressShipmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
